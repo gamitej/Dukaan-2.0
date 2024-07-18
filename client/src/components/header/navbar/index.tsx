14b43b417ec4c94d873f225cb5796717697bec1c
gamitej <gamitejpratapsingh998@gmail.com>
2024-07-18 12:11:46 +0530

    NEW : navbar header link added

--- client/src/components/header/navbar/index.tsx
+++ client/src/components/header/navbar/index.tsx
@@ -1,13 +1,19 @@
 import { AiOutlineMenu, AiFillProduct } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   /**
    * TSX
    */
   return (
     <div className="w-full sticky top-0 bg-dark z-[10] flex justify-between items-center h-[5rem] px-[50px]">
       {/* left  */}
-      <div className="flex justify-center items-center gap-6">
+      <div
+        className="flex justify-center items-center gap-6 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
         <AiOutlineMenu className="text-white text-2xl" />
         <h2 className="text-white text-2xl">DASHBOARD</h2>
       </div>
