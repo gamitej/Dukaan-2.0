d88cc2e1ea027a586ed0e3c393a923dabf899cc2
gamitej <gamitejpratapsingh998@gmail.com>
2024-07-23 19:50:54 +0530

    NEW :  common table cols data

---
+++ client/src/data/CommonTable.ts
@@ -0,0 +1,27 @@
+/**
+ * ================= SALES & PURCHASE COLS ================
+ */
+
+// column for sales & purchase table data
+export const commonCols = [
+  //   { header: "Order Id", accessorkey: "order_id" },
+  { header: "Date", accessorkey: "date" },
+  { header: "Product", accessorkey: "product" },
+  { header: "Product Category", accessorkey: "category" },
+  { header: "Product Company", accessorkey: "company" },
+  { header: "Quanity", accessorkey: "qunatity" },
+  { header: "Weight", accessorkey: "weight" },
+  { header: "Price", accessorkey: "price" },
+];
+
+/**
+ * ================= RETURN COLS ================
+ */
+
+export const returnCols = [
+  { header: "Order Id", accessorkey: "order_id" },
+  { header: "Date", accessorkey: "date" },
+  { header: "Quanity", accessorkey: "qunatity" },
+  { header: "Weight", accessorkey: "weight" },
+  { header: "Price", accessorkey: "price" },
+];
