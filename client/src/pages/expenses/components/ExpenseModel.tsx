2c520b4ca39f42bf68703369a4315389d6c31728
gamitej <gamitejpratapsingh998@gmail.com>
2024-07-30 15:20:11 +0530

    BUILD : expense form done

--- client/src/pages/expenses/components/ExpenseModel.tsx
+++ client/src/pages/expenses/components/ExpenseModel.tsx
@@ -1,8 +1,51 @@
+import dayjs from "dayjs";
+import { ChangeEvent, FormEvent } from "react";
+// components
+import Dropdown from "@/components/fields/Dropdown";
 import BasicModal from "@/components/model/BasicModel";
+import InputField from "@/components/fields/InputField";
+import BasicDatePicker from "@/components/fields/BasicDatePicker";
+// store
 import { useExpenseStore } from "@/store/expenseStore";
 
+const dropDownOptions = [
+  { label: "Salary", value: "salary" },
+  { label: "Auto Fair", value: "Auto-fair" },
+  { label: "Paledar", value: "paledar" },
+  { label: "Fuel / Petrol", value: "petrol" },
+  { label: "Shop Rent", value: "shop rent" },
+  { label: "Others", value: "others" },
+];
+
 const ExpenseModel = () => {
-  const { isModelOpen, setIsModelOpen } = useExpenseStore();
+  const {
+    isModelOpen,
+    setIsModelOpen,
+    formData,
+    setFormData,
+    setIsFormValid,
+    isFormValid,
+  } = useExpenseStore();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(name, value);
+    setIsFormValid();
+  };
+
+  const handleChangeDropDown = (name: string, value: string) => {
+    setFormData(name, value);
+    setIsFormValid();
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // setReset();
+
+    // const { name, shop, contact } = formData;
+
+    // mutateAddPartNameData({ party_name: name, shop_name: shop, contact });
+  };
 
   /**
    * TSX
@@ -16,8 +59,53 @@ const ExpenseModel = () => {
         onClose={setIsModelOpen}
         title="Add Expenses"
       >
-        <form>
-            
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <div className="flex justify-center items-start gap-3">
+            <BasicDatePicker
+              width="100%"
+              value={formData.date}
+              setDateChange={(value) =>
+                setFormData("date", dayjs(value).format("YYYY-MM-DD"))
+              }
+            />
+            <InputField
+              id={"amount"}
+              width={"100%"}
+              label={"Amount"}
+              type={"number"}
+              value={formData.amount}
+              handleChange={handleInputChange}
+              placeholder={"enter amount..."}
+            />
+          </div>
+          <div className="flex justify-center items-start gap-3">
+            <InputField
+              id={"description"}
+              width={"70%"}
+              label={"Description"}
+              value={formData.description}
+              handleChange={handleInputChange}
+              placeholder={"enter description..."}
+            />
+            <Dropdown
+              id="expense_type"
+              width={"30%"}
+              options={dropDownOptions}
+              label={"Expense Type"}
+              value={formData.expense_type}
+              setInputChange={(value: string) => {
+                handleChangeDropDown("expense_type", value);
+              }}
+            />
+          </div>
+
+          <button
+            type="submit"
+            disabled={!isFormValid}
+            className="disabled:bg-slate-300 bg-mediumDark text-white py-3 rounded-sm shadow-md"
+          >
+            submit
+          </button>
         </form>
       </BasicModal>
     </div>
--- client/src/store/expenseStore.ts
+++ client/src/store/expenseStore.ts
@@ -1,8 +1,31 @@
+import dayjs from "dayjs";
 import { create } from "zustand";
+
+const defaultFormData = {
+  amount: 0,
+  description: "",
+  expense_type: "",
+  date: dayjs().format("YYYY-MM-DD"),
+};
+
+export type FormDataExpense = {
+  expense_type: string;
+  description: string;
+  amount: number;
+  date: string;
+};
 
 interface ExpenseState {
   isModelOpen: boolean;
   setIsModelOpen: () => void;
+
+  formData: FormDataExpense;
+  setFormData: (name: string, value: string | number) => void;
+
+  isFormValid: boolean;
+  setIsFormValid: () => void;
+
+  setResetFormData: () => void;
 }
 
 export const useExpenseStore = create<ExpenseState>((set) => ({
@@ -11,4 +34,39 @@ export const useExpenseStore = create<ExpenseState>((set) => ({
   setIsModelOpen: () => {
     set((state) => ({ ...state, isModelOpen: !state.isModelOpen }));
   },
+
+  formData: defaultFormData,
+  setFormData: (key, value) =>
+    set((state) => ({
+      formData: { ...state.formData, [key]: value },
+    })),
+
+  setResetFormData: () => {
+    set((state) => ({
+      ...state,
+      isModelOpen: false,
+      formData: defaultFormData,
+    }));
+  },
+
+  isFormValid: false,
+  setIsFormValid: () =>
+    set((state) => {
+      const { formData } = state;
+
+      const validations = Object.entries(formData).reduce<
+        Record<string, boolean>
+      >((acc, [key, value]) => {
+        if (key === "amount") {
+          acc[key] = value.toString().length > 0;
+        } else {
+          acc[key] = value.toString().trim().length > 0;
+        }
+        return acc;
+      }, {});
+
+      const isFormValid = Object.values(validations).every(Boolean);
+
+      return { isFormValid };
+    }),
 }));
