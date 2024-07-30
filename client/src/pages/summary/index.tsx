9acfe129017a003fbea4bfb81ff1ffd6bc37a106
gamitej <gamitejpratapsingh998@gmail.com>
2024-07-30 21:34:51 +0530

    NEW : select tab component created

--- client/src/pages/summary/index.tsx
+++ client/src/pages/summary/index.tsx
@@ -1,3 +1,4 @@
+import SelectTab from "./(components)/SelectTab";
 import SummaryCard from "./(components)/SummaryCard";
 
 const Summary = () => {
@@ -6,10 +7,11 @@ const Summary = () => {
    */
   return (
     <div className="w-[90%] m-auto mt-10">
-      <div className="text-white text-2xl text-center bg-lightDark py-4">
+      <div className="text-white text-2xl bg-lightDark py-4 flex justify-center items-center gap-4">
         Summary
       </div>
       <SummaryCard />
+      <SelectTab />
     </div>
   );
 };
